ce6fa44d1830aab4fce4eafd8f99bbc43e330334
dianamar34 <santos24garcia@unitec.edu>
2018-11-13 14:00:35 -0600

    Java app updated

--- demo/src/main/java/hello/HelloWorld.java
+++ demo/src/main/java/hello/HelloWorld.java
@@ -1,6 +1,11 @@
 package hello;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+
+@RestController
 public class HelloWorld {
+    @RequestMapping("/")
     public static void main(String[] args) {
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
