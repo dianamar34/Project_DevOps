694b32617687359ea34d0e63eb3d63d46ad0e8ae
Diana Santos <santos24garcia@unitec.edu>
2018-11-23 10:52:02 -0600

    New App

---
+++ SpringBootMySQL/src/main/java/com/diana/CityRepository.java
@@ -0,0 +1,10 @@
+package com.diana;
+
+import com.diana.City;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends CrudRepository<City, Long> {
+
+}
---
+++ SpringBootMySQL/src/main/java/com/diana/Service/ICityService.java
@@ -0,0 +1,9 @@
+package com.diana.Service;
+
+import java.util.List;
+import com.diana.City;
+
+public interface ICityService {
+
+    public List<City> findAll();
+}
